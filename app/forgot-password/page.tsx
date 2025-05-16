614fd2166dcb0fcbbcbd901018033736198228bc
Astitv Singh <astitvsingh+github@outlook.com>
2025-05-16 18:44:29 +0530

    [feat #0](github): Add GitHub CLI scripts for label management and authentication
    - Introduced scripts for adding labels, creating predefined labels, and authenticating the GitHub CLI.
    - The `addLabel` script allows users to add labels to issues or pull requests with error handling.
    - The `createLabels` script automates the creation of a comprehensive set of labels for the repository.
    - The `auth` script facilitates GitHub CLI authentication with full scope access.
    - Updated the `setup` script to integrate the new label creation and authentication processes.
    
    These additions streamline GitHub repository management and enhance user experience.
    
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    	new file:   .github/gh/addLabel
    	new file:   .github/gh/auth
    	modified:   .github/gh/create-fr
    	new file:   .github/gh/createLabels
    	modified:   .github/gh/setup
    	new file:   app/forgot-password/index.ts
    	new file:   app/forgot-password/page.tsx
    	new file:   app/forgot-password/styles.module.css
    	new file:   app/forgot-password/view.tsx

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+// Copyright 2025 © Astitv Singh <https://github.com/astitvsingh>.
+// SPDX-License-Identifier: MIT
+
+"use client"; // Enables client-side rendering in Next.js 13+ app directory
+import { SignupView } from "@/app";
+
+export default SignupView;
